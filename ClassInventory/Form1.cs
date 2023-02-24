eb116d0ffb1955d5b8449da687436e5bf5ee0a1d
Tony Theodoropoulos <theodot@outlook.com>
2023-02-24 08:58:34 -0500

    Add project files.

---
+++ ClassInventory/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassInventory
+{
+    public partial class Form1 : Form
+    {
+        // Global Variabales go here
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            // This is to be completed in Part II. You will use 
+            // Lambda Expressions. 
+            //---------------------------
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
